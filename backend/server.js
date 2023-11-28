86735553864582f411c2520c52706fcd3fd1b7a8
nm1008 <mallari08nikko@yahoo.com>
2023-11-29 03:24:07 +0800

    cors changes in backend part 2

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      `simply-book.vercel.app/`,
+      `https://deploy-mern-frontend-jet.vercel.app/`,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
